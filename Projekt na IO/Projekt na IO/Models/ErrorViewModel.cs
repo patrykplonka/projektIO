a685968f7f5db8723a10390c986d7fc9499c6b87
patrykplonka <123236364+patrykplonka@users.noreply.github.com>
2023-10-26 17:02:16 +0200

    initial commit

---
+++ Projekt na IO/Projekt na IO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projekt_na_IO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
